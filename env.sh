0ef2213a05e45c387e38a542076e8e7cf48189de
Aaron Weiss <aaronweiss8@gmail.com>
2020-11-27 15:16:53 -0500

    initial push on branch

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export C_INCLUDE_PATH=$HOME/opt-dev/include
+export CPLUS_INCLUDE_PATH=$HOME/opt-dev/include
+export LIBRARY_PATH=$HOME/opt-dev/lib
+export LD_LIBRARY_PATH=$HOME/opt-dev/lib
+export CMAKE_PREFIX_PATH=$HOME/opt-dev/
